fca8c25478eaec7da6ae56c9b3a35395a699a643
Vadim Meshcheryakov <painassasin@icloud.com>
2021-09-15 20:34:51 +0500

    Небольшой рефакторинг

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from main import db
 # POST
 
 class Post(db.Model):
-    __tablename__='posts'
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(50))
     content = db.Column(db.String(1000))
@@ -16,7 +16,7 @@ class Post(db.Model):
 # COMMENT
 
 class Comment(db.Model):
-    __tablename__='comments'
+    __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(100))
     content = db.Column(db.String(1000))
