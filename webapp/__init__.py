583121ea916e3f5c96727353ca58ac3fb9cdd3a3
Vadim Meshcheryakov <painassasin@icloud.com>
2021-09-16 11:58:19 +0500

    Убрал файл с конфигами

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from config import Config
-
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/sqlite3.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
