07844f69a0b82bbd63932630b0c1b4effbee45ba
Vadim Meshcheryakov <painassasin@icloud.com>
2021-09-18 17:41:01 +0500

    Изменил ссылку на базу и установил psycopg2

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,7 +3,12 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/sqlite3.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = ('postgres://azzyswikpgmtfu:' +
+                                         '9c2f4c36cdc270cba8cdb00f51' +
+                                         'a1e5dd5acba094fcd9898878f6' +
+                                         '979ce848eca0@ec2-54-158-247' +
+                                         '-97.compute-1.amazonaws.com:' +
+                                         '5432/d951cg60mur8eo')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
